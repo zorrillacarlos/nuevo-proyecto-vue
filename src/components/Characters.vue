362deabd259574f75d7b66e5d872ea561a7b61ff
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2022-05-04 06:47:00 -0400

    add documentation axios and fetch data

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -2,8 +2,16 @@
   <div class="container mb-5">
     <h1 class="text-center mb-5">CHARACTERS RICK AND MORTY</h1>
     <div class="row justify-content-center">
-      <div v-for="(item, index) in result" :key="index" class="card col-6 col-sm-2 m-3 p-0">
-        <img v-bind:src="item.image" class="card-img-top" v-bind:alt="item.name"/>
+      <div
+        class="card col-6 col-sm-2 m-3 p-0"
+        v-for="(item, index) in result"
+        :key="index"
+      >
+        <img
+          class="card-img-top"
+          v-bind:src="item.image"
+          v-bind:alt="item.name"
+        />
         <div class="card-body">
           <h5 class="card-title">{{ item.name }}</h5>
           <p class="card-text">Origen: {{ item.origin.name }}</p>
@@ -15,15 +23,21 @@
 </template>
 
 <script>
+
+// Axios
 // https://codingpotions.com/vue-axios
 // https://es.vuejs.org/v2/guide/list.html
+
+// Cotenido dinamico v-bind
 // https://www.youtube.com/watch?v=B8rVlxQm8Cs
+
+//Fetch Data
 // https://youtu.be/7iDGJolHFmU
 
 //import axios from "axios";
 
 export default {
-/*   data: () => ({
+  /*   data: () => ({
     result: null,
   }),
   created() {
@@ -32,18 +46,18 @@ export default {
     });
   }, */
 
-  data(){
+  data() {
     return {
-      result: null
-      }
+      result: null,
+    };
   },
-  mounted(){
-      fetch("https://rickandmortyapi.com/api/character")
-      .then(res => res.json())
-      .then(data => this.result = data.results)
-  }
-};
 
+  mounted() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => (this.result = data.results));
+  },
+};
 </script>
 
 <style></style>
